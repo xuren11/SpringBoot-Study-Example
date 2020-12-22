261750e9591a2b690bce3ed3a384595a15356365
xuren11 <296685262@qq.com>
2020-12-22 22:25:02 +0800

    bigfile test

---
+++ src/main/java/com/xuren/study/controller/BigFileDownloagController.java
@@ -0,0 +1,11 @@
+package com.xuren.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/big")
+public class BigFileDownloagController {
+
+
+}
